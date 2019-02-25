273df4f29a6d5a12339839ec64197afab8cd1eed
Y.J. Moens <yoeri.moens@hva.nl>
2019-02-25 21:35:40 +0100

    [Feature] Controller action for getting courses added. Sends XML to the view

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Course;
 use Illuminate\View\View;
+use SimpleXMLElement;
 
 class CourseController extends Controller
 {
     public function index(): View
     {
-        $courses = Course::all();
+        $courses = Course::query()->with('lectures')
+            ->get();
 
-        return view('courses', ['courses' => $courses]);
+        $xmlElement = new SimpleXMLElement('<rootTag/>');
+        $xmlCourses = $this->to_xml($xmlElement, $courses->toArray());
+
+        return view('course.index', compact('xmlCourses'));
     }
 }
