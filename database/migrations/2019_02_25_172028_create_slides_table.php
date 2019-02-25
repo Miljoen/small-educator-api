90ef36c341791dd265843b7767881b43dde985d0
Y.J. Moens <yoeri.moens@hva.nl>
2019-02-25 18:23:51 +0100

    [Feature] Slides migration added

---
+++ database/migrations/2019_02_25_172028_create_slides_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('lecture_id')->unsigned();
+            $table->foreign('lecture_id')
+                ->references('id')
+                ->on('lectures')
+                ->onDelete('RESTRICT');
+            $table->string('title');
+            $table->text('content');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
