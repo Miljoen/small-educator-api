e77d5d6e973dcf1fcab3f69675f6893ffe883855
Y.J. Moens <yoeri.moens@hva.nl>
2019-02-25 18:32:04 +0100

    [Feature] Questions table migration added

---
+++ database/migrations/2019_02_25_172632_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('slide_id')->unsigned();
+            $table->foreign('slide_id')
+                ->references('id')
+                ->on('slides')
+                ->onDelete('RESTRICT');
+            $table->text('question_text');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
