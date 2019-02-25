3979ceae17136bc24e1acbf0ae7898d4ec07510f
Y.J. Moens <yoeri.moens@hva.nl>
2019-02-25 18:19:02 +0100

    [Update] Course relation added

--- app/Course.php
+++ app/Course.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
@@ -10,7 +11,16 @@ class Course extends Model
      * @var string[]
      */
     protected $fillable = [
-        'name',
+        'title',
         'description'
     ];
+
+    // ------------------------------------------------------------------------------
+    //      Relations
+    // ------------------------------------------------------------------------------
+
+    public function lectures(): HasMany
+    {
+        return $this->hasMany(Lecture::class);
+    }
 }
