1e0eb70adc61bf1992414f7ba3745f03cf440f19
moens <yoerimoens@gmail.com>
2019-02-26 10:42:26 +0100

    [Update] slide text fields pivot table added

---
+++ database/migrations/2019_02_26_093520_slide_text_field.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SlideTextField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slide_text_field', function (Blueprint $table) {
+            $table->integer('slide_id')
+                ->unsigned();
+            $table->foreign('slide_id')
+                ->references('id')
+                ->on('slides')
+                ->onDelete('RESTRICT');
+
+            $table->integer('text_field_id')
+                ->unsigned();
+            $table->foreign('text_field_id')
+                ->references('id')
+                ->on('text_fields')
+                ->onDelete('RESTRICT');
+
+            $table->float('time_on_screen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
