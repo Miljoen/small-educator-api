5916ac81029bb214ae964338fc0faca59b4a0c9e
moens <yoerimoens@gmail.com>
2019-03-01 14:52:54 +0100

    [Update] LectureController show action altered to xml response

--- app/Http/Controllers/LectureController.php
+++ app/Http/Controllers/LectureController.php
@@ -3,48 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Lecture;
-use Illuminate\View\View;
 use Spatie\ArrayToXml\ArrayToXml;
+use Symfony\Component\HttpFoundation\Response;
 
 class LectureController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
         $lectures = Lecture::query()->with('slides')
             ->first();
-        $lecturesArray = (['SmallEducator' => $lectures->toArray()]);
+
+        $lecturesArray = ([
+            'SmallEducator' => $lectures->toArray(),
+        ]);
 
         $xmlLectures = ArrayToXml::convert($lecturesArray);
 
         return response()->xml($xmlLectures);
     }
 
-    public function show(int $id): View
+    public function show(int $id): Response
     {
         $lecture = Lecture::where('id', $id)->with('slides')
             ->get();
 
-        $xmlElement = new SimpleXMLElement('<rootTag/>');
-        $xmlLecture = $this->toXML($xmlElement, $lecture->toArray());
+        $lectureArray = ([
+            'SmallEducator' => $lecture->toArray(),
+        ]);
 
-        return view('lecture.show', compact('xmlLecture'));
-    }
+        $xmlLectures = ArrayToXml::convert($lectureArray);
 
-    /** TODO: Refactor xml responses */
-    function toXML(SimpleXMLElement $object, array $data): SimpleXMLElement
-    {
-        foreach ($data as $key => $value) {
-            if (is_array($value)) {
-                $new_object = $object->addChild($key);
-                $this->toXML($new_object, $value);
-            } else {
-                if ($key == (int) $key) {
-                    $key = "key_$key";
-                }
-
-                $object->addChild($key, $value);
-            }
-        }
-        return $object;
+        return response()->xml($xmlLectures);
     }
 }
