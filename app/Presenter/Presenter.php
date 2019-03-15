f1ad810e4418fe453e9161cff300d17cfa38623d
moens <yoerimoens@gmail.com>
2019-03-15 11:35:38 +0100

    [Feature] Presenter parent class getModels function added

--- app/Presenter/Presenter.php
+++ app/Presenter/Presenter.php
@@ -2,9 +2,16 @@
 
 namespace App\Presenter;
 
+use App\Modules\Course\Course;
+
 abstract class Presenter
 {
     protected $models;
 
     abstract function  __construct(int $id = null);
+
+    /**
+     * @return Course[]|Course
+     */
+    abstract function getModels();
 }
