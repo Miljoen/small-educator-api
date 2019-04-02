2bdfdea2744782e8e441c183dcfe9cadd7a724d8
Rick van den Berg <>
2019-04-02 10:34:37 +0200

    [Updated] CoursePresenter Methods

--- app/Modules/Course/CoursePresenter.php
+++ app/Modules/Course/CoursePresenter.php
@@ -3,34 +3,40 @@
 namespace App\Modules\Course;
 
 use App\Presenter\Presenter;
+use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
-class CoursePresenter extends Presenter
-{
+class CoursePresenter extends Presenter {
     /** @var Collection $models */
     protected $models;
 
-    public function __construct(int $id = null)
-    {
+    public function __construct(int $id = NULL) {
         $id ? $this->getCourse($id) : $this->getCourses();
     }
 
-    public function getCourses()
-    {
+    public function getCourses() {
         $this->models = Course::query()->withLectures()->get();
     }
 
-    public function getCourse(int $id)
-    {
+    public function getCourse(int $id) {
         $this->models = Course::withLectures()
             ->findOrNew($id);
+    }
+
+    public function store(Request $request) {
+
+        $course = Course::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return new Course($course);
     }
 
     /**
      * @return Collection|Course
      */
-    function getModels()
-    {
+    function getModels() {
         return $this->models;
     }
 }
